0cd24cbccc652d79edc84d4efc94195b5b89403b
Rafael Ribeiro <rafael@ascitech.com.br>
2025-06-26 20:32:10 -0300

    fix: register typescript eslint plugin

--- frontend/eslint.config.cjs
+++ frontend/eslint.config.cjs
@@ -1,13 +1,19 @@
 // frontend/eslint.config.cjs
 /** @type {import('eslint').FlatConfig[]} */
-const { configs: { recommended: eslintRecommended } } = require("eslint");
-const { configs: { recommended: tsRecommended, "recommended-requiring-type-checking": tsTypeChecked } } = require("@typescript-eslint/eslint-plugin");
+const { configs: { recommended: eslintRecommended } } = require("@eslint/js");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
 const tsParser = require("@typescript-eslint/parser");
-const { configs: { recommended: reactRecommended } } = require("eslint-plugin-react");
-const { configs: { recommended: jsxA11yRecommended } } = require("eslint-plugin-jsx-a11y");
-const { configs: { recommended: reactHooksRecommended } } = require("eslint-plugin-react-hooks");
-const { configs: { recommended: importRecommended } } = require("eslint-plugin-import");
-const { configs: { "prettier-recommended": prettierRecommended } } = require("eslint-plugin-prettier");
+const reactPlugin = require("eslint-plugin-react");
+const jsxA11yPlugin = require("eslint-plugin-jsx-a11y");
+const reactHooksPlugin = require("eslint-plugin-react-hooks");
+const importPlugin = require("eslint-plugin-import");
+const prettierPlugin = require("eslint-plugin-prettier");
+const { configs: { recommended: tsRecommended, "recommended-requiring-type-checking": tsTypeChecked } } = tsPlugin;
+const { configs: { recommended: reactRecommended } } = reactPlugin;
+const { configs: { recommended: jsxA11yRecommended } } = jsxA11yPlugin;
+const { configs: { recommended: reactHooksRecommended } } = reactHooksPlugin;
+const { configs: { recommended: importRecommended } } = importPlugin;
+const { configs: { recommended: prettierRecommended } } = prettierPlugin;
 
 module.exports = [
   // ESLint core
@@ -15,7 +21,8 @@ module.exports = [
 
   // TS basic
   {
-    ...tsRecommended,
+    plugins: { "@typescript-eslint": tsPlugin },
+    rules: tsRecommended.rules,
     languageOptions: {
       parser: tsParser,
       parserOptions: {
@@ -30,7 +37,8 @@ module.exports = [
 
   // TS com type-checking
   {
-    ...tsTypeChecked,
+    plugins: { "@typescript-eslint": tsPlugin },
+    rules: tsTypeChecked.rules,
     languageOptions: {
       parser: tsParser,
       parserOptions: {
@@ -42,25 +50,40 @@ module.exports = [
 
   // React
   {
-    ...reactRecommended,
-    settings: { react: { version: "detect" } },
+    plugins: { react: reactPlugin },
     rules: { ...reactRecommended.rules, "react/react-in-jsx-scope": "off" },
+    languageOptions: { parserOptions: reactRecommended.parserOptions },
+    settings: { react: { version: "detect" } },
   },
 
   // A11y
-  jsxA11yRecommended,
+  {
+    plugins: { "jsx-a11y": jsxA11yPlugin },
+    rules: jsxA11yRecommended.rules,
+    languageOptions: {
+      parserOptions: jsxA11yRecommended.parserOptions,
+    },
+  },
 
   // Hooks
-  reactHooksRecommended,
+  {
+    plugins: { "react-hooks": reactHooksPlugin },
+    rules: reactHooksRecommended.rules,
+  },
 
   // Import
   {
-    ...importRecommended,
+    plugins: { import: importPlugin },
+    rules: importRecommended.rules,
+    languageOptions: { parserOptions: importRecommended.parserOptions },
     settings: { "import/resolver": { typescript: {} } },
   },
 
   // Prettier
-  prettierRecommended,
+  {
+    plugins: { prettier: prettierPlugin },
+    rules: prettierRecommended.rules,
+  },
 
   // ignores e patterns
   {
